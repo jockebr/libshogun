7f1d65d6badf78b68d32d02a5e8fab25f65e0ef8
Pika <panic@thedis.co>
2018-09-06 17:08:55 -0400

    woah

--- shogun.go
+++ shogun.go
@@ -2,9 +2,57 @@ package libshogun
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"errors"
-	"json"
+	"io/ioutil"
 	"net/http"
 )
 
-// TODO: do stuffs!
+func NewShogunClient(shopn_cert, shopn_key, dauth_token string) (client *ShogunClient, err error) {
+	shopn, err := tls.LoadX509KeyPair(shopn_cert, shopn_key)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ShogunClient{
+		&http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					Certificates: []tls.Certificate{shopn},
+				},
+			},
+		},
+		dauth_token,
+	}, nil
+}
+
+func (c *ShogunClient) DoRequest(endpoint string) (response string, err error) {
+	req, err := http.NewRequest("GET", "https://bugyo.hac.lp1.eshop.nintendo.net/shogun/v1"+endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("X-DeviceAuthorization", c.dauth_token)
+
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	return bytes, nil
+}
+
+func (c *ShogunClient) GetNsId(tid int) (ns int, err error) {
+	resp, err := c.DoRequest("/contents/ids?shop_id=4&lang=en&country=US&type=title&title_ids=" + tid)
+	if err != nil {
+		return nil, err
+	}
+
+	data := &NsRequestResponse{}
+	json.Unmarshal(resp, data)
+
+	return data.id_pair.ns_id, nil
+}
---
+++ structs.go
@@ -0,0 +1,15 @@
+package libshogun
+
+type ShogunClient struct {
+	http        *http.Client
+	dauth_token string
+}
+
+type NsRequestResponse struct {
+	id_pair *IdPair `json:"id_pairs"`
+}
+
+type IdPair struct {
+	ns_id    int `json:"id"`
+	title_id int `json:"title_id"`
+}
