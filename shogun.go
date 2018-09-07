3c732b05ef7701d6e6306c05be5dfdadd112a5b8
Pika <panic@thedis.co>
2018-09-07 18:01:35 -0400

    r e a l l y  c u t e

--- shogun.go
+++ shogun.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 )
 
+// NewShogunClient creates a new ShogunClient
 func NewShogunClient(shopn_cert, shopn_key, dauth_token string) (client *ShogunClient, err error) {
 	shopn, err := tls.LoadX509KeyPair(shopn_cert, shopn_key)
 	if err != nil {
@@ -27,15 +28,16 @@ func NewShogunClient(shopn_cert, shopn_key, dauth_token string) (client *ShogunC
 	}, nil
 }
 
-func (c *ShogunClient) DoRequest(endpoint string) (response []byte, err error) {
-	req, err := http.NewRequest("GET", "https://bugyo.hac.lp1.eshop.nintendo.net/shogun/v1"+endpoint, nil)
+// DoRequest sends a request to the specified URL with the proper authentication
+func (c *ShogunClient) DoRequest(url string) (response []byte, err error) {
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	req.Header.Set("X-DeviceAuthorization", c.dauth_token)
+	req.Header.Set("X-DeviceAuthorization", c.DauthToken)
 
-	resp, err := c.http.Do(req)
+	resp, err := c.HTTP.Do(req)
 	if err != nil {
 		return nil, err
 	}
@@ -46,8 +48,29 @@ func (c *ShogunClient) DoRequest(endpoint string) (response []byte, err error) {
 	return bytes, nil
 }
 
+// DoShogunRequest sends a request to the specified Shogun endpoint with the proper authentication
+func (c *ShogunClient) DoShogunRequest(endpoint string) (response []byte, err error) {
+	req, err := http.NewRequest("GET", "https://bugyo.hac.lp1.eshop.nintendo.net/shogun/v1"+endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("X-DeviceAuthorization", c.DauthToken)
+
+	resp, err := c.HTTP.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	return bytes, nil
+}
+
+// GetNsId returns the NS ID for the given Title ID
 func (c *ShogunClient) GetNsId(tid string) (ns int64, err error) {
-	resp, err := c.DoRequest("/contents/ids?shop_id=4&lang=en&country=US&type=title&title_ids=" + tid)
+	resp, err := c.DoShogunRequest("/contents/ids?shop_id=4&lang=en&country=US&type=title&title_ids=" + tid)
 	if err != nil {
 		return 0, err
 	}
@@ -60,38 +83,115 @@ func (c *ShogunClient) GetNsId(tid string) (ns int64, err error) {
 	return id, nil
 }
 
-func (c *ShogunClient) GetTitleData(ns_id int64) (title Title) {
-	resp, _ := c.DoRequest("/titles/" + strconv.FormatInt(ns_id, 10) + "?shop_id=4&lang=en&country=US")
-	var (
-		id           int
-		name         string
-		banner_url   string
-		release_date string
-		is_new       bool
-		is_dlc       bool
-		description  bool
-		genre        string
-		size         int64
-		screenshots  []string
-		movies       []string
-		publisher    Publisher
-	)
-
-	values := [][]string{
-		[]string{"id"},
-		[]string{"formal_name"},
-		[]string{"hero_banner_url"},
-		[]string{"release_date"},
-		[]string{"is_new"},
-		[]string{"is_dlc"},
-		[]string{"description"},
-		[]string{"total_rom_size"},
-		[]string{"publisher"},
+// GetTitleData returns a Title instance for the given NS ID
+func (c *ShogunClient) GetTitleData(ns_id int64) (title *Title, err error) {
+	resp, err := c.DoShogunRequest("/titles/" + strconv.FormatInt(ns_id, 10) + "?shop_id=4&lang=en&country=US")
+	if err != nil {
+		return &Title{}, err
 	}
 
-	jsonparser.EachKey(data, func(index int, value []byte) {
-		switch index {
+	id, err := jsonparser.GetInt(resp, "id")
+	if err != nil {
+		return &Title{}, err
+	}
 
-		}
-	}, values...)
+	name, err := jsonparser.GetString(resp, "formal_name")
+	if err != nil {
+		return &Title{}, err
+	}
+
+	banner_url, err := jsonparser.GetString(resp, "hero_banner_url")
+	if err != nil {
+		return &Title{}, err
+	}
+	banner_url = "https://bugyo.hac.lp1.eshop.nintendo.net" + banner_url
+
+	release_date, err := jsonparser.GetString(resp, "release_date_on_eshop")
+	if err != nil {
+		return &Title{}, err
+	}
+
+	is_new, err := jsonparser.GetBoolean(resp, "is_new")
+	if err != nil {
+		return &Title{}, err
+	}
+
+	is_dlc, err := jsonparser.GetBoolean(resp, "in_app_purchase")
+	if err != nil {
+		return &Title{}, err
+	}
+
+	description, err := jsonparser.GetString(resp, "description")
+	if err != nil {
+		return &Title{}, err
+	}
+
+	genre, err := jsonparser.GetString(resp, "genre")
+	if err != nil {
+		return &Title{}, err
+	}
+
+	size, err := jsonparser.GetInt(resp, "total_rom_size")
+	if err != nil {
+		return &Title{}, err
+	}
+
+	screenshots := []string{}
+	jsonparser.ArrayEach(resp, func(value []byte, value_type jsonparser.ValueType, offset int, err error) {
+		// todo: add error checking
+		screenshots = append(screenshots, "https://bugyo.hac.lp1.eshop.nintendo.net"+string(value))
+	}, "images", "url")
+
+	movies := []*Movie{}
+	jsonparser.ArrayEach(resp, func(value []byte, value_type jsonparser.ValueType, offset int, err error) {
+		// todo: add error checking
+		url, _ := jsonparser.GetString(value, "movie_url")
+		thumbnail_url, _ := jsonparser.GetString(value, "thumbnail_url")
+
+		movies = append(movies, &Movie{
+			URL:       "https://bugyo.hac.lp1.eshop.nintendo.net" + url,
+			Thumbnail: "https://bugyo.hac.lp1.eshop.nintendo.net" + thumbnail_url,
+		})
+	}, "movies")
+
+	pub_id, err := jsonparser.GetInt(resp, "publisher", "id")
+	if err != nil {
+		return &Title{}, err
+	}
+
+	pub_name, err := jsonparser.GetString(resp, "publisher", "name")
+	if err != nil {
+		return &Title{}, err
+	}
+
+	title_id, err := jsonparser.GetString(resp, "applications", "[0]", "id")
+	if err != nil {
+		return &Title{}, err
+	}
+
+	icon_url, err := jsonparser.GetString(resp, "applications", "[0]", "image_url")
+	if err != nil {
+		return &Title{}, err
+	}
+	icon_url = "https://bugyo.hac.lp1.eshop.nintendo.net" + icon_url
+
+	return &Title{
+		id,
+		name,
+		banner_url,
+		release_date,
+		is_new,
+		is_dlc,
+		description,
+		genre,
+		size,
+		screenshots,
+		movies,
+		&Publisher{
+			pub_id,
+			pub_name,
+		},
+		title_id,
+		icon_url,
+	}, nil
 }
--- structs.go
+++ structs.go
@@ -4,27 +4,38 @@ import (
 	"net/http"
 )
 
+// ShogunClient repersents a client to interact with Shogun
 type ShogunClient struct {
-	http        *http.Client
-	dauth_token string
+	HTTP       *http.Client
+	DauthToken string
 }
 
+// Title repersents a title in Shogun
 type Title struct {
-	id int
-	name
-	banner_url
-	release_date string
-	is_new
-	is_dlc bool
-	description
-	genre       string
-	size        int64
-	screenshots []string
-	movies      []string
-	publisher   Publisher
+	ID          int64
+	Name        string
+	BannerURL   string
+	ReleaseDate string
+	IsNew       bool
+	IsDLC       bool
+	Description string
+	Genre       string
+	Size        int64
+	Screenshots []string
+	Movies      []*Movie
+	Publisher   *Publisher
+	TitleID     string
+	IconURL     string
 }
 
+// Movie repersents a movie
+type Movie struct {
+	URL       string
+	Thumbnail string
+}
+
+// Publisher repersents a title's publisher
 type Publisher struct {
-	id   int
-	name string
+	ID   int64
+	Name string
 }
